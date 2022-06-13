c224328759b7f4263083c43c9b1891d206c300a6
Jason <jayemcee121@gmail.com>
2022-06-13 10:38:39 -0400

    Basic functionality

---
+++ src/App.js
@@ -0,0 +1,25 @@
+
+import React from 'react';
+import './App.css';
+import SearchBar from './components/searchBar';
+import Home from './components/home';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import resultPage from './components/resultPage';
+
+function App() {
+  return (
+    <BrowserRouter>
+      <div className='App'>
+        <h1 style={{textAlign: 'center'}}>Search for Hacker News!</h1>
+        <SearchBar />
+        <Switch>
+          <Route path="/" exact={true} component={Home} />
+          <Route path='/results/:result_id' component={resultPage} />
+          {/* <Route path='/testData' component={} /> */}
+        </Switch>
+      </div>
+    </BrowserRouter>
+  );
+}
+
+export default App;
